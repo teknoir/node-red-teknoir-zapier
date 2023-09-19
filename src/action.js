e3934e29f25f7a42e582003ed7562cc6993fc55c
cristianTeknoir <cristian.manaila@teknoir.ai>
2023-09-19 17:03:34 +0300

    Update action.js

--- src/action.js
+++ src/action.js
@@ -28,10 +28,18 @@ module.exports = function (RED) {
 
     RED.nodes.registerType("zap_action", action);
 
-    RED.httpNode.use('/zap/action/*', bodyParser.json());
+    RED.httpNode.use('/_zap/action/*', bodyParser.json());
 
-    RED.httpNode.post('/zapr/action/:id', function (req, res) {
+    RED.httpNode.post('/_zap/action/:id', function (req, res) {
         let target_node = RED.nodes.getNode(req.params.id)
+        
+        console.log("Received headers:", req.headers);  
+        console.log("Received body:", req.body);  
+
+        if (!req.body) {
+            return res.status(400).send("Bad Request: Missing body");
+        }
+        
         if (target_node) {
             target_node.emit('newAction', req.body)
             res.send(req.body);
